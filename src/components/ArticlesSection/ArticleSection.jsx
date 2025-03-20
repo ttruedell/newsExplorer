b6f7341efa4a828b58cbb9c205803ff3ca687a3f
Tyson Truedell <truedet@yahoo.com>
2025-03-19 22:03:04 -0400

    removing unecessary components from ArticleSection

--- src/components/ArticlesSection/ArticleSection.jsx
+++ src/components/ArticlesSection/ArticleSection.jsx
@@ -1,17 +1,13 @@
 import "./ArticleSection.css";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
 import NewsCard from "../NewsCard/NewsCard";
 
 function ArticleSection() {
   return (
     <main>
-      <Header />
       <p>Saved articles </p>
       <h1>Elise, you have 5 saved articles</h1>
       <span>keywords: filler, filler, filler</span>
       <NewsCard />
-      <Footer />
     </main>
   );
 }
