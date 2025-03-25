9ddd880ede70f3294432d1df3568357c8f801d24
Tyson Truedell <truedet@yahoo.com>
2025-03-24 20:42:10 -0400

    adding classnames to  SearchForm elements

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -3,14 +3,18 @@ import "./SearchForm.css";
 function SearchForm() {
   return (
     <div>
-      <form action="">
-        <h1>What's going on in the world?</h1>
-        <p>
+      <form action="" className="search-form">
+        <h1 className="search-form__header">What's going on in the world?</h1>
+        <p className="search-form__subheader">
           Find the latest news on any topic and save then in your personal
           account.
         </p>
         <div>
-          <input type="text" placeholder="Enter topic" />
+          <input
+            type="text"
+            placeholder="Enter topic"
+            className="search-form__input"
+          />
           <button>Search</button>
         </div>
       </form>
