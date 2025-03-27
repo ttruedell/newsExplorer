baddb7d7d9813da9f062cfc13a1e8741c15a8b77
Tyson Truedell <truedet@yahoo.com>
2025-03-26 22:42:57 -0400

    removing commented-out code; modifying Footer element styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,18 +7,18 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__copyright">@ 2025 Supersite, Powered by News API</p>
-      <div>
-        <ul className="footer__links">
-          <li className="footer__link">Home</li>
-          <li className="footer__link">TripleTen</li>
-          <li className="footer__link">
-            <img src={github} alt="github-icon" />
-          </li>
-          <li className="footer__link">
-            <img src={social} alt="social-icon" />
-          </li>
-        </ul>
-      </div>
+      {/* <div> */}
+      <ul className="footer__links">
+        <li className="footer__link footer__link_home">Home</li>
+        <li className="footer__link footer__link_TripleTen">TripleTen</li>
+        <li className="footer__link footer__link_github">
+          <img src={github} alt="github-icon" />
+        </li>
+        <li className="footer__link footer__link_facebook">
+          <img src={social} alt="social-icon" />
+        </li>
+      </ul>
+      {/* </div> */}
     </footer>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,14 +16,8 @@ function Header() {
         <Link to="/" className={isHome ? "header__logo" : "header-saved__logo"}>
           <p>NewsExplorer</p>
         </Link>
-        {/* <Link to="/">
-        <button>Home</button>
-      </Link>
-      <button>Sign In</button> */}
-
         <Navigation />
       </div>
-      {/* <hr className="header__border" width="100%" color="#C4C4C4" size="1px" /> */}
     </header>
   );
 }
