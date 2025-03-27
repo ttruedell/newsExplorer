1f9180d18e81ef43f8ef7b26cea1b83c27efedca
Tyson Truedell <truedet@yahoo.com>
2025-03-26 22:16:04 -0400

    modifying Header/Navigation components with temporary styles logic (isHome)

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,25 +2,39 @@ import "./Navigation.css";
 import { Link } from "react-router-dom";
 
 import CurrentUserContext from "../../contexts/CurrentUserContext";
-import { useContext } from "react";
+// import { useContext } from "react";
+import { useLocation } from "react-router-dom";
+
+import signout from "../../assets/sign-out-icon.jpg";
 
 function Navigation() {
-  const currentUser = useContext(CurrentUserContext);
+  const isHome = location.pathname === "/";
 
   return (
     <div className="navigation">
-      {currentUser ? (
-        <Link to="/profile" className="navigation__links">
-          <div className="navigation__user-container">
-            <button>Home</button>
-            <button>Saved Articles</button>
-            <button>{currentUser.name}</button>
-          </div>
-        </Link>
+      {isHome ? (
+        <div className="navigation__non-user">
+          <Link to="/">
+            <button className="navigation__home_signed-out">Home</button>
+          </Link>
+          <button className="navigation__sign-in">Sign In</button>
+        </div>
       ) : (
-        <div className="navigation__sign-in">
-          <button className="navigation__home-button">Home</button>
-          <button className="navigation__sign-in-button">Sign In</button>
+        <div className="navigation__user">
+          <Link to="/">
+            <button className="navigation__home_signed-in">Home</button>
+          </Link>
+          <Link to="/saved-news" /*className="navigation__saved-news-button"*/>
+            <button className="navigation__saved-news">Saved Articles</button>
+          </Link>
+          <button className="navigation__sign-out">
+            Elise
+            <img
+              className="navigation__sign-out-icon"
+              src={signout}
+              alt="sign-out"
+            />
+          </button>
         </div>
       )}
     </div>
