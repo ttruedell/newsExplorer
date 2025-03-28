a45298236f6bf915445aa9f835d8785337e720e0
Tyson Truedell <truedet@yahoo.com>
2025-03-27 22:32:56 -0400

    applying styles to news-card placeholder and news-cards container

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -13,70 +13,68 @@ function NewsCard() {
   const [showConfirm, setShowConfirm] = useState(false);
 
   return (
-    <li className="news-card">
-      {isHome ? (
-        <div className="news-card__header-container">
-          <h2 className="news-card__confirm-bookmark">
-            Sign in to save articles
-          </h2>
+    <div className="news-cards">
+      <li className="news-card">
+        {isHome ? (
+          <div className="news-card__header-container">
+            <h2 className="news-card__confirm-bookmark">
+              Sign in to save articles
+            </h2>
 
-          <button className="news-card__bookmark-btn">
-            <img
-              className="news-card__bookmark-icon"
-              src={bookmark}
-              alt="remove-icon"
-            />
-          </button>
+            <button className="news-card__bookmark-btn">
+              <img
+                className="news-card__bookmark-icon"
+                src={bookmark}
+                alt="remove-icon"
+              />
+            </button>
+          </div>
+        ) : (
+          <div className="news-card__header-container">
+            <h2 className="news-card__keyword">example</h2>
+            <h2
+              className="news-card__confirm-remove"
+              style={{
+                opacity: showConfirm ? 1 : 0,
+                transition: "opacity 0.5s ease-in-out",
+              }}
+            >
+              Remove from saved
+            </h2>
+
+            <button className="news-card__remove-btn">
+              <img
+                onMouseEnter={() => setShowConfirm(true)}
+                onMouseLeave={() => setShowConfirm(false)}
+                className="news-card__remove-icon"
+                src={remove}
+                alt="remove-icon"
+              />
+            </button>
+          </div>
+        )}
+
+        <div className="news-card__content">
+          <img
+            className="news-card__image"
+            src={placeholder}
+            alt="article-image"
+          />
+          <article className="news-card__article">
+            <p className="news-card__date">November 8, 2024</p>
+            <h2 className="news-card__title">Placeholder</h2>
+            <p className="news-card__text">
+              Lorem ipsum dolor sit amet. Ut repellendus voluptatum sed unde
+              dignissimos qui quos reprehenderit aut neque ipsa. At officiis
+              exercitationem quod qui molestias consectetur id odio nihil ea
+              sint voluptates. Et voluptas quam est ducimus quae et voluptatem
+              quam vel numquam optio a tempore ducimus?
+            </p>
+            <h3 className="news-card__author">National Geographic</h3>
+          </article>
         </div>
-      ) : (
-        <div className="news-card__header-container">
-          <h2 className="news-card__keyword">example</h2>
-          <h2
-            className="news-card__confirm-remove"
-            style={{
-              opacity: showConfirm ? 1 : 0,
-              transition: "opacity 0.5s ease-in-out",
-            }}
-          >
-            Remove from saved
-          </h2>
-
-          <button className="news-card__remove-btn">
-            <img
-              onMouseEnter={() => setShowConfirm(true)}
-              onMouseLeave={() => setShowConfirm(false)}
-              className="news-card__remove-icon"
-              src={remove}
-              alt="remove-icon"
-            />
-          </button>
-        </div>
-      )}
-
-      <div className="news-card__content">
-        <img
-          className="news-card__image"
-          src={placeholder}
-          alt="article-image"
-        />
-        <article className="news-card__article">
-          <p className="news-card__date">November 8, 2024</p>
-          <h2 className="news-card__title">Placeholder</h2>
-          <p className="news-card__text">
-            Lorem ipsum dolor sit amet. Ut repellendus voluptatum sed unde
-            dignissimos qui quos reprehenderit aut neque ipsa. At officiis
-            officiis eum voluptates provident ad voluptates officia hic officia
-            numquam. Est error nemo ut dolorem porro sit fugit neque ea ratione
-            voluptate ea quod possimus et sunt laborum. Ab exercitationem quod
-            qui molestias consectetur id odio nihil ea sint voluptates. Et
-            voluptas quam est ducimus quae et voluptatem quam vel numquam optio
-            a tempore ducimus?
-          </p>
-          <h3 className="news-card__author">National Geographic</h3>
-        </article>
-      </div>
-    </li>
-    // </section>
+      </li>
+    </div>
   );
 }
 
