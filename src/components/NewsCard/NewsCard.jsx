5244d6ca14f561718386a70ae86cf837c35bb8d9
Tyson Truedell <truedet@yahoo.com>
2025-03-28 21:42:46 -0400

    creating foundational grid layout for news-cards

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -81,6 +81,219 @@ function NewsCard() {
           </article>
         </div>
       </li>
+      {/* ///////////// */}
+      {/* ///////////// */}
+      {/* ///////////// */}
+      {/* ///////////// */}
+      <li className="news-card">
+        {isHome ? (
+          <div className="news-card__header-container">
+            <h2 className="news-card__confirm-bookmark">
+              Sign in to save articles
+            </h2>
+
+            <button className="news-card__bookmark-btn">
+              <img
+                className="news-card__bookmark-icon"
+                src={bookmark}
+                alt="remove-icon"
+              />
+            </button>
+          </div>
+        ) : (
+          <div className="news-card__header-container">
+            <h2 className="news-card__keyword">example</h2>
+            <h2
+              className="news-card__confirm-remove"
+              style={{
+                opacity: showConfirm ? 1 : 0,
+                transition: "opacity 0.5s ease-in-out",
+              }}
+            >
+              Remove from saved
+            </h2>
+
+            <button
+              className="news-card__remove-btn"
+              onMouseEnter={() => setShowConfirm(true)}
+              onMouseLeave={() => setShowConfirm(false)}
+            >
+              <img
+                className="news-card__remove-icon"
+                style={{
+                  opacity: showConfirm ? 0.5 : 1,
+                  transition: "opacity 0.5s ease-in-out",
+                }}
+                src={remove}
+                alt="remove-icon"
+              />
+            </button>
+          </div>
+        )}
+
+        <div className="news-card__content">
+          <img
+            className="news-card__image"
+            src={placeholder}
+            alt="article-image"
+          />
+          <article className="news-card__article">
+            <p className="news-card__date">November 8, 2024</p>
+            <h2 className="news-card__title">
+              Nostalgic Photos of Tourists in U.S. National Parks
+            </h2>
+            <p className="news-card__text">
+              Lorem ipsum dolor sit amet. Ut repellendus voluptatum sed unde
+              dignissimos qui quos reprehenderit aut neque ipsa. At officiis
+              sint voluptates. Et voluptas quam est ducimus quae et voluptatem
+              quam vel numquam optio a tempore ducimus?
+            </p>
+            <h3 className="news-card__author">National Geographic</h3>
+          </article>
+        </div>
+      </li>
+      {/* ///////////// */}
+      {/* ///////////// */}
+      {/* ///////////// */}
+      {/* ///////////// */}
+      <li className="news-card">
+        {isHome ? (
+          <div className="news-card__header-container">
+            <h2 className="news-card__confirm-bookmark">
+              Sign in to save articles
+            </h2>
+
+            <button className="news-card__bookmark-btn">
+              <img
+                className="news-card__bookmark-icon"
+                src={bookmark}
+                alt="remove-icon"
+              />
+            </button>
+          </div>
+        ) : (
+          <div className="news-card__header-container">
+            <h2 className="news-card__keyword">example</h2>
+            <h2
+              className="news-card__confirm-remove"
+              style={{
+                opacity: showConfirm ? 1 : 0,
+                transition: "opacity 0.5s ease-in-out",
+              }}
+            >
+              Remove from saved
+            </h2>
+
+            <button
+              className="news-card__remove-btn"
+              onMouseEnter={() => setShowConfirm(true)}
+              onMouseLeave={() => setShowConfirm(false)}
+            >
+              <img
+                className="news-card__remove-icon"
+                style={{
+                  opacity: showConfirm ? 0.5 : 1,
+                  transition: "opacity 0.5s ease-in-out",
+                }}
+                src={remove}
+                alt="remove-icon"
+              />
+            </button>
+          </div>
+        )}
+
+        <div className="news-card__content">
+          <img
+            className="news-card__image"
+            src={placeholder}
+            alt="article-image"
+          />
+          <article className="news-card__article">
+            <p className="news-card__date">November 8, 2024</p>
+            <h2 className="news-card__title">
+              Nostalgic Photos of Tourists in U.S. National Parks
+            </h2>
+            <p className="news-card__text">
+              Lorem ipsum dolor sit amet. Ut repellendus voluptatum sed unde
+              dignissimos qui quos reprehenderit aut neque ipsa. At officiis
+              sint voluptates. Et voluptas quam est ducimus quae et voluptatem
+              quam vel numquam optio a tempore ducimus?
+            </p>
+            <h3 className="news-card__author">National Geographic</h3>
+          </article>
+        </div>
+      </li>
+      {/* ///////////// */}
+      {/* ///////////// */}
+      {/* ///////////// */}
+      {/* ///////////// */}
+      <li className="news-card">
+        {isHome ? (
+          <div className="news-card__header-container">
+            <h2 className="news-card__confirm-bookmark">
+              Sign in to save articles
+            </h2>
+
+            <button className="news-card__bookmark-btn">
+              <img
+                className="news-card__bookmark-icon"
+                src={bookmark}
+                alt="remove-icon"
+              />
+            </button>
+          </div>
+        ) : (
+          <div className="news-card__header-container">
+            <h2 className="news-card__keyword">example</h2>
+            <h2
+              className="news-card__confirm-remove"
+              style={{
+                opacity: showConfirm ? 1 : 0,
+                transition: "opacity 0.5s ease-in-out",
+              }}
+            >
+              Remove from saved
+            </h2>
+
+            <button
+              className="news-card__remove-btn"
+              onMouseEnter={() => setShowConfirm(true)}
+              onMouseLeave={() => setShowConfirm(false)}
+            >
+              <img
+                className="news-card__remove-icon"
+                style={{
+                  opacity: showConfirm ? 0.5 : 1,
+                  transition: "opacity 0.5s ease-in-out",
+                }}
+                src={remove}
+                alt="remove-icon"
+              />
+            </button>
+          </div>
+        )}
+
+        <div className="news-card__content">
+          <img
+            className="news-card__image"
+            src={placeholder}
+            alt="article-image"
+          />
+          <article className="news-card__article">
+            <p className="news-card__date">November 8, 2024</p>
+            <h2 className="news-card__title">
+              Nostalgic Photos of Tourists in U.S. National Parks
+            </h2>
+            <p className="news-card__text">
+              Lorem ipsum dolor sit amet. Ut repellendus voluptatum sed unde
+              dignissimos qui quos reprehenderit aut neque ipsa. At officiis
+              sint voluptates. Et voluptas quam est ducimus quae et voluptatem
+              quam vel numquam optio a tempore ducimus?
+            </p>
+            <h3 className="news-card__author">National Geographic</h3>
+          </article>
+        </div>
+      </li>
     </div>
   );
 }
