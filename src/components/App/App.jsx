4678a953c4606ab113fef7d4daaaaed9b493219b
Tyson Truedell <truedet@yahoo.com>
2025-04-01 14:29:32 -0400

    creating event handlers Login and Register Modals

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,6 +11,50 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [activeModal, setActiveModal] = useState("");
+
+  const handleSignUpClick = () => {
+    setActiveModal("register");
+  };
+
+  const handleLoginClick = () => {
+    setActiveModal("login");
+  };
+
+  const closeActiveModal = (event) => {
+    if (!event) {
+      return setActiveModal("");
+    }
+
+    if (handleModalClose(event)) {
+      setActiveModal("");
+    }
+  };
+
+  const handleModalClose = (event) => {
+    if (event.type === "keydown" && event.key === "Escape") {
+      closeActiveModal();
+    } else if (event.type === "click") {
+      if (
+        (!event.target.closest(".modal__content") &&
+          event.target.classList.contains("modal")) ||
+        event.target.closest(".modal__close-btn")
+      ) {
+        closeActiveModal();
+      }
+    }
+  };
+
+  const handleUserModal = () => {
+    if (activeModal === "register") {
+      closeActiveModal();
+      setActiveModal("login");
+    }
+    if (activeModal === "login") {
+      closeActiveModal();
+      setActiveModal("register");
+    }
+  };
 
   // function changeHeader {
   //   if (loggedIn) {
