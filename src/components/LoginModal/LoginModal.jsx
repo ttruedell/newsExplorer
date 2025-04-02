2473204d9295f0e3d50f26f6c5dd3353cd24ae13
Tyson Truedell <truedet@yahoo.com>
2025-04-01 20:50:16 -0400

    creating layout for LoginModal with useState hooks declared

---
+++ src/components/LoginModal/LoginModal.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+
+const LoginModal = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
+  return <div></div>;
+};
