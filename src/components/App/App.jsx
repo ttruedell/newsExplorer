2dba843df3db9c46796cfcca358deecc165294be
Tyson Truedell <truedet@yahoo.com>
2025-04-04 19:32:04 -0400

    including useNavigate for handleLogout to route to home page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 import "./App.css";
 
@@ -47,9 +47,12 @@ function App() {
     }, 500);
   };
 
+  const navigate = useNavigate();
+
   const handleLogout = () => {
     setCurrentUser(null);
     setLoggedIn(false);
+    navigate("/");
   };
 
   const closeActiveModal = (event) => {
