971cfa7811ff5236a8401212cdf0a24de617630e
Tyson Truedell <truedet@yahoo.com>
2025-04-05 01:18:39 -0400

    style: making NewsCard grid, About, and Footer responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,12 +6,13 @@ function About() {
     <section className="about">
       <img src={creator} alt="creator_image" className="about__image" />
       <div className="about__content">
-        <h1>About the Author</h1>
-        <p>
+        <h1 className="about__header">About the Author</h1>
+        <p className="about__descritption">
           This block describes the project author. Here you should indicate your
           name, what you do, and which development technologies you know.
         </p>
-        <p>
+        <br />
+        <p className="about__descritption">
           You can also talk about your experience with TripleTen, what you
           learned there, and how you can help potential customers.
         </p>
