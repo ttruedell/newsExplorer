8a5bb070f6480e551e25267dba91b32fd8a5380d
Tyson Truedell <truedet@yahoo.com>
2025-04-07 18:19:17 -0400

    Fix: ensuring footer sticks to bottom and page content fills height

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -25,4 +25,13 @@ export const initialNewsCards = [
     text: "Lorem ipsum dolor sit amet. Ut repellendus voluptatum sed undedignissimos qui quos reprehenderit aut neque ipsa. At officiisofficiis eum voluptates provident ad voluptates officia hic officianumquam. Est error nemo ut dolorem porro sit fugit neque ea rationevoluptate ea quod possimus et sunt laborum. Ab exercitationem quodqui molestias consectetur id odio nihil ea sint voluptates. Etvoluptas quam est ducimus quae et voluptatem quam vel numquam optioa tempore ducimus?",
     author: "The Wire",
   },
+
+  // {
+  //   date: "November 6, 2024",
+  //   title: "Placeholder 1",
+  //   link: "url-placeholder",
+  //   image: placeholderImage,
+  //   text: "Lorem ipsum dolor sit amet. Ut repellendus voluptatum sed undedignissimos qui quos reprehenderit aut neque ipsa. At officiisofficiis eum voluptates provident ad voluptates officia hic officianumquam. Est error nemo ut dolorem porro sit fugit neque ea rationevoluptate ea quod possimus et sunt laborum. Ab exercitationem quodqui molestias consectetur id odio nihil ea sint voluptates. Etvoluptas quam est ducimus quae et voluptatem quam vel numquam optioa tempore ducimus?",
+  //   author: "National Geographic",
+  // },
 ];
