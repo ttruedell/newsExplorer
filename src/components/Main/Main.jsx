cd022b3bb3109e86a3e7affc39a8a58052d44ca8
Tyson Truedell <truedet@yahoo.com>
2025-04-08 20:36:49 -0400

    creating loading, found-results, and no-results html/css for Preloader

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import "./Main.css";
 import SearchForm from "../SearchForm/SearchForm";
 import About from "../About/About";
+import Preloader from "../Preloader/Preloader";
 
 function Main() {
   return (
     <main className="main">
       <SearchForm />
+      <Preloader />
       <About />
     </main>
   );
--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,10 +1,38 @@
 import "./Preloader.css";
-import ArticleSection from "../ArticlesSection/ArticleSection";
+import ellipse from "../../assets/ellipse.svg";
+import notFound from "../../assets/not-found.svg";
+import { initialNewsCards } from "../../utils/constants";
+import NewsCard from "../NewsCard/NewsCard";
 
 function Preloader() {
   return (
-    <div>
-      <ArticleSection />
+    <div className="preloader">
+      {/* <div className="prelaoder__loading">
+        <img
+          className="preloader__circle-preloader"
+          src={ellipse}
+          alt="loading"
+        />
+        <p className="prelaoder__text">Searching for news...</p>
+      </div> */}
+
+      {/* <div className="preloader__reuslts">
+        <div className="prelaoder__no-results">
+          <img className="preloader__icon" src={notFound} alt="loading" />
+          <h2 className="preloader__header">Nothing Found</h2>
+          <p className="prelaoder__text">Sorry, but nothing matched</p>
+          <p className="prelaoder__text"> your results.</p>
+        </div>
+      </div> */}
+
+      <div className="prelaoder__results">
+        <h2 className="news-cards__results-header">Search Results</h2>
+        <ul className="news-cards">
+          {initialNewsCards.map((card, index) => (
+            <NewsCard key={index} card={card} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
