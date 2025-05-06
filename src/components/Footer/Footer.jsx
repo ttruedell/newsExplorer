bca410177d41a2993b20b68a9026049e30f0111c
Tyson Truedell <truedet@yahoo.com>
2025-05-05 22:54:00 -0400

    updating footer link functionality and destinations

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import "./Footer.css";
 
+import { Link } from "react-router-dom";
+
 import github from "../../assets/github.svg";
 import social from "../../assets/fb.svg";
 
@@ -9,8 +11,19 @@ function Footer() {
       <p className="footer__copyright">@ 2025 Supersite, Powered by News API</p>
       <ul className="footer__links">
         <div className="footer__row-one">
-          <li className="footer__link footer__link_home">Home</li>
-          <li className="footer__link footer__link_TripleTen">TripleTen</li>
+          <li>
+            <Link to="/" className="footer__link footer__link_home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <a
+              href="https://tripleten.com/"
+              className="footer__link footer__link_TripleTen"
+            >
+              TripleTen
+            </a>
+          </li>
           {/* <li className="footer__link footer__link_github">
           <img src={github} alt="github-icon" />
         </li>
@@ -20,7 +33,9 @@ function Footer() {
         </div>
         <div className="footer__row-two">
           <li className="footer__link footer__link_github">
-            <img src={github} alt="github-icon" />
+            <a href="https://github.com/ttruedell/newsExplorer">
+              <img src={github} alt="github-icon" />
+            </a>
           </li>
           <li className="footer__link footer__link_facebook">
             <img src={social} alt="social-icon" />
